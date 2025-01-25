7180d74de8eed581a18a1ba58dcd537639f4cace
Rami Haidar <haidar_rami@hotmail.com>
2025-01-25 21:21:14 +0100

    Change server port from 3000 to 4000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const port = 3000;
+const port = 4000;
 
 const db = new sqlite3.Database('./database.sqlite');
 
